1b2a3b6b62b02b2f58672cd269746df818bc53fe
Willian Rodrigues <86743131+WILLRDNK@users.noreply.github.com>
2022-02-22 10:07:57 -0300

    Criado classe que abstrai algumas funções do puppteer

---
+++ modules/onuHunter/index.js
@@ -0,0 +1,31 @@
+import { launch } from 'puppeteer';
+
+class BotOnuHunter{
+
+
+    async newBrowser(){
+        const browser = await launch( {headless:true, ignoreHTTPSErrors: true}  );
+        return browser
+    }
+    
+    async newPage(){
+        const browser = await this.newBrowser();
+        const page = await browser.newPage();
+        return page
+    }
+
+    async goTo(url){
+        const page = await this.newPage()
+        const sla = await page.goto(url,{timeout:10000});
+        return sla
+    }
+
+    async exit(){
+        const browser = await this.newBrowser()
+        const exit = await browser.close()
+        return exit
+    }
+
+}
+
+export default BotOnuHunter
