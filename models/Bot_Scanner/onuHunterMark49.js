8c5b4addfb2a6d68df546fe27efb500d2ac960f5
Willian Rodrigues <86743131+WILLRDNK@users.noreply.github.com>
2022-03-08 13:21:58 -0300

    Ajustes

--- models/Bot_Scanner/onuHunterMark49.js
+++ models/Bot_Scanner/onuHunterMark49.js
@@ -1,11 +1,10 @@
 const OnuHunter = require('../../modules/OnuHunter/OnuHunter');
-
 const onuHunter = new OnuHunter
 
 async function scanerTesteAcessoRemoto(ip){
     await onuHunter.newBrowser(true);
     await onuHunter.newPage();
-    await onuHunter.scanner(ip)
+    await onuHunter.scanner(ip);
     const log = await onuHunter.title();
     await onuHunter.closeBrowser();
     return log
